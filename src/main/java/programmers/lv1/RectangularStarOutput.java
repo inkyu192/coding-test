f89e21dddcc60d7cfe8b2eecf4a7a9d9dee04fab
inkyu192 <inkyu192@gmail.com>
2022-07-20 20:04:41 +0900

    코드 수정

--- src/main/java/programmers/lv1/RectangularStarOutput.java
+++ src/main/java/programmers/lv1/RectangularStarOutput.java
@@ -6,7 +6,6 @@ public class RectangularStarOutput {
 
     public void solution(int a, int b) {
         StringBuffer star = new StringBuffer();
-
         IntStream.range(0, a).forEach(i -> star.append("*"));
         IntStream.range(0, b).forEach(i -> System.out.println(star.toString()));
     }
