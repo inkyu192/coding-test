c936936933d22754763719ca43e02b6ea3944f70
inkyu192 <inkyu192@gmail.com>
2022-07-23 08:38:26 +0900

    콜라츠 추측

---
+++ src/main/java/programmers/lv1/CollatzGuess.java
@@ -0,0 +1,15 @@
+package programmers.lv1;
+
+public class CollatzGuess {
+
+    public int solution(int num) {
+        long n = num;
+
+        for (int i = 0; i < 500; i++) {
+            if (n == 1) return i;
+            n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+        }
+
+        return -1;
+    }
+}
