82dc54b3b50310076411202f1bce61e1037e7755
inkyu192 <inkyu192@gmail.com>
2022-07-26 00:05:16 +0900

    정수 내림차순으로 배치하기

---
+++ src/test/java/programmers/lv1/PlacingIntegersInDescendingOrderTest.java
@@ -0,0 +1,21 @@
+package programmers.lv1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("정수 내림차순으로 배치하기")
+class PlacingIntegersInDescendingOrderTest {
+
+    PlacingIntegersInDescendingOrder placingIntegersInDescendingOrder = new PlacingIntegersInDescendingOrder();
+
+    @Test
+    void testCase1() {
+        int n = 118372;
+
+        long expected = 873211;
+
+        assertThat(placingIntegersInDescendingOrder.solution(n)).isEqualTo(expected);
+    }
+}
