05e9cf20e526b94bac2a74a23b01626d5fd167f0
inkyu192 <inkyu192@gmail.com>
2022-08-01 00:02:29 +0900

    로또의 최고 순위와 최저 순위

---
+++ src/main/java/programmers/lv1/TheHighestAndLowestRanksOfTheLottery.java
@@ -0,0 +1,45 @@
+package programmers.lv1;
+
+import java.util.Arrays;
+import java.util.stream.LongStream;
+
+public class TheHighestAndLowestRanksOfTheLottery {
+
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int maxCount = 0;
+        int minCount = 0;
+
+        for (int i = 0; i < lottos.length; i++) {
+            for (int j = 0; j < win_nums.length; j++) {
+                if (lottos[i] == win_nums[j]) {
+                    maxCount++;
+                    minCount++;
+                }
+            }
+            if (lottos[i] == 0) maxCount++;
+        }
+
+        answer[0] = getRank(maxCount);
+        answer[1] = getRank(minCount);
+
+        return answer;
+    }
+
+    private int getRank(int count) {
+        switch (count) {
+            case 6:
+                return 1;
+            case 5:
+                return 2;
+            case 4:
+                return 3;
+            case 3:
+                return 4;
+            case 2:
+                return 5;
+            default:
+                return 6;
+        }
+    }
+}
