dde6d8d1df1f29b0f6093cf7bab18d87f3e360e5
inkyu192 <inkyu192@gmail.com>
2022-08-02 09:38:44 +0900

    3진법 뒤집기

---
+++ src/main/java/programmers/lv1/TernaryNumeralSystemFlipOver.java
@@ -0,0 +1,12 @@
+package programmers.lv1;
+
+public class TernaryNumeralSystemFlipOver {
+
+    public int solution(int n) {
+        StringBuilder sb = new StringBuilder(Integer.toString(n, 3));
+        sb.reverse();
+        int answer = Integer.parseInt(sb.toString(), 3);
+
+        return answer;
+    }
+}
