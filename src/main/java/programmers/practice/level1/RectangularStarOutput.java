29e3cd88b186317ac7bf641cf002d9a3c4f3755a
inkyu192 <inkyu192@gmail.com>
2022-08-07 00:05:20 +0900

    직사각형 별찍기

---
+++ src/main/java/programmers/practice/level1/RectangularStarOutput.java
@@ -0,0 +1,12 @@
+package programmers.practice.level1;
+
+import java.util.stream.IntStream;
+
+public class RectangularStarOutput {
+
+    public void solution(int a, int b) {
+        StringBuilder star = new StringBuilder();
+        IntStream.range(0, a).forEach(i -> star.append("*"));
+        IntStream.range(0, b).forEach(i -> System.out.println(star.toString()));
+    }
+}
---
+++ src/test/java/programmers/practice/level1/RectangularStarOutputTest.java
@@ -0,0 +1,20 @@
+package programmers.practice.level1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("직사각형 별찍기")
+class RectangularStarOutputTest {
+
+    RectangularStarOutput rectangularStarOutput = new RectangularStarOutput();
+
+    @Test
+    void testCase1() {
+        rectangularStarOutput.solution(5, 3);
+    }
+
+    @Test
+    void testCase2() {
+        rectangularStarOutput.solution(2, 2);
+    }
+}
