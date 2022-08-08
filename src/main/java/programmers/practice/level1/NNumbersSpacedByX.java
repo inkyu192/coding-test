22f9c4e69edf8bfe1f5cabf2edff08663396c25f
inkyu192 <inkyu192@gmail.com>
2022-08-09 00:14:39 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ src/main/java/programmers/practice/level1/NNumbersSpacedByX.java
@@ -0,0 +1,14 @@
+package programmers.practice.level1;
+
+public class NNumbersSpacedByX {
+
+    public long[] solution(long x, int n) {
+        long[] answer = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = x * (i + 1);
+        }
+
+        return answer;
+    }
+}
