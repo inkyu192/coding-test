1bf177cdd0e8f44b87fb141c079f9ca4e28e4bd0
inkyu192 <inkyu192@gmail.com>
2022-08-10 00:01:01 +0900

    핸드폰 번호 가리기

---
+++ src/test/java/programmers/practice/level1/HideCellPhoneNumberTest.java
@@ -0,0 +1,30 @@
+package programmers.practice.level1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("핸드폰 번호 가리기")
+class HideCellPhoneNumberTest {
+
+    HideCellPhoneNumber hideCellPhoneNumber = new HideCellPhoneNumber();
+
+    @Test
+    void testCase1() {
+        String phone_number = "01033334444";
+
+        String expected = "*******4444";
+
+        assertThat(hideCellPhoneNumber.solution(phone_number)).isEqualTo(expected);
+    }
+
+    @Test
+    void testCase2() {
+        String phone_number = "027778888";
+
+        String expected = "*****8888";
+
+        assertThat(hideCellPhoneNumber.solution(phone_number)).isEqualTo(expected);
+    }
+}
