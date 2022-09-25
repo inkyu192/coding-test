b5b060a2e10569110a679820b912984cff30e60a
inkyu192 <inkyu192@gmail.com>
2022-09-25 22:42:40 +0900

    평균 구하기

--- src/main/java/programmers/practice/level1/Average.java
+++ src/main/java/programmers/practice/level1/Average.java
@@ -1,10 +1,14 @@
 package programmers.practice.level1;
 
-import java.util.Arrays;
-
 public class Average {
 
     public double solution(int[] arr) {
-        return Arrays.stream(arr).average().orElse(0.0);
+        int answer = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+
+        return (double) answer / arr.length;
     }
 }
