12ed3448cb33cbf649f9175dbbf4106d28913256
inkyu192 <inkyu192@gmail.com>
2022-10-04 00:40:11 +0900

    콜라츠 추측

--- src/main/java/programmers/practice/level1/CollatzGuess.java
+++ src/main/java/programmers/practice/level1/CollatzGuess.java
@@ -7,7 +7,7 @@ public class CollatzGuess {
 
         for (int i = 0; i < 500; i++) {
             if (n == 1) return i;
-            n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+            n = (n % 2 == 0) ? n/2 : n * 3 + 1;
         }
 
         return -1;
