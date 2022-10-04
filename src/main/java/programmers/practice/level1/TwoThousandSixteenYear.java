b0732d4e5aadd12a6be30ea92c6ed69f6e3528b9
inkyu192 <inkyu192@gmail.com>
2022-10-05 00:49:04 +0900

    2016년

--- src/main/java/programmers/practice/level1/TwoThousandSixteenYear.java
+++ src/main/java/programmers/practice/level1/TwoThousandSixteenYear.java
@@ -1,10 +1,14 @@
 package programmers.practice.level1;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class TwoThousandSixteenYear {
 
     public String solution(int a, int b) {
-        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
+        LocalDate localDate = LocalDate.of(2016, a, b);
+        DayOfWeek day = localDate.getDayOfWeek();
+
+        return day.toString().substring(0, 3);
     }
 }
