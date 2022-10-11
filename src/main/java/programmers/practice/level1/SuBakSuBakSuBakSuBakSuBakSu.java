827e8ce3604459a1cfb7019b66a93eec112797d4
inkyu192 <inkyu192@gmail.com>
2022-10-11 23:48:57 +0900

    수박수박수박수박수박수?

--- src/main/java/programmers/practice/level1/SuBakSuBakSuBakSuBakSuBakSu.java
+++ src/main/java/programmers/practice/level1/SuBakSuBakSuBakSuBakSuBakSu.java
@@ -3,12 +3,12 @@ package programmers.practice.level1;
 public class SuBakSuBakSuBakSuBakSuBakSu {
 
     public String solution(int n) {
-        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
-            answer.append(i % 2 == 0 ? "수" : "박");
+            sb.append(i % 2 == 0 ? "수" : "박");
         }
 
-        return answer.toString();
+        return sb.toString();
     }
 }
