891d70299de39bdb885f03923c0ad7ea6be8b085
inkyu192 <inkyu192@gmail.com>
2022-10-14 20:58:41 +0900

    가운데 글자 가져오기

--- src/main/java/programmers/practice/level1/GetMiddleLetter.java
+++ src/main/java/programmers/practice/level1/GetMiddleLetter.java
@@ -3,14 +3,6 @@ package programmers.practice.level1;
 public class GetMiddleLetter {
 
     public String solution(String s) {
-        String answer = "";
-
-        if (s.length() % 2 == 0) {
-            answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
-        } else {
-            answer = s.substring(s.length() / 2, s.length() / 2 + 1);
-        }
-
-        return answer;
+        return s.substring((s.length() - 1) / 2, s.length() / 2 + 1);
     }
 }
