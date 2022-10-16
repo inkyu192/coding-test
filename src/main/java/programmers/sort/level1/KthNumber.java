d1cd5b144c30bd422e69e4d7ceacc762abbdd834
inkyu192 <inkyu192@gmail.com>
2022-10-16 23:52:28 +0900

    K번째수

--- src/main/java/programmers/sort/level1/KthNumber.java
+++ src/main/java/programmers/sort/level1/KthNumber.java
@@ -5,12 +5,16 @@ import java.util.Arrays;
 public class KthNumber {
 
     public int[] solution(int[] array, int[][] commands) {
+        int index = 0;
         int[] answer = new int[commands.length];
 
         for (int i = 0; i < commands.length; i++) {
             int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+
             Arrays.sort(temp);
-            answer[i] = temp[commands[i][2] - 1];
+
+            answer[index] = temp[commands[i][2] - 1];
+            index++;
         }
 
         return answer;
