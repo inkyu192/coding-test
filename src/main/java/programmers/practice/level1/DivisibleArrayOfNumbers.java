063e9b936fb545d1e92b7f97c6b55e91860a4978
inkyu192 <inkyu192@gmail.com>
2022-10-20 00:15:44 +0900

    나누어 떨어지는 숫자 배열

--- src/main/java/programmers/practice/level1/DivisibleArrayOfNumbers.java
+++ src/main/java/programmers/practice/level1/DivisibleArrayOfNumbers.java
@@ -5,8 +5,25 @@ import java.util.Arrays;
 public class DivisibleArrayOfNumbers {
 
     public int[] solution(int[] arr, int divisor) {
-        int[] answer = Arrays.stream(arr).filter(i -> i % divisor == 0).sorted().toArray();
-        if (answer.length == 0) return new int[]{-1};
+        int length = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) length++;
+        }
+
+        if (length == 0) return new int[]{-1};
+
+        int index = 0;
+        int[] answer = new int[length];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                answer[index] = arr[i];
+                index++;
+            }
+        }
+
+        Arrays.sort(answer);
 
         return answer;
     }
