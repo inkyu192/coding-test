9a57210fb774daa08435ee8417b97548d2781ff7
inkyu192 <inkyu192@gmail.com>
2022-10-24 00:19:48 +0900

    [1차] 다트 게임

--- src/main/java/programmers/kakao/level1/FirstDartGame.java
+++ src/main/java/programmers/kakao/level1/FirstDartGame.java
@@ -3,42 +3,38 @@ package programmers.kakao.level1;
 public class FirstDartGame {
 
     public int solution(String dartResult) {
+        int index = 0;
         String temp = "";
-        int[] arr = new int[3];
-        int idx = 0;
+        int[] score = {0, 0, 0};
 
         for (int i = 0; i < dartResult.length(); i++) {
-            switch (dartResult.charAt(i)) {
-                case '*':
-                    arr[idx - 1] *= 2;
-                    if (idx > 1) arr[idx - 2] *= 2;
-                    break;
-                case '#':
-                    arr[idx - 1] *= -1;
-                    break;
-                case 'S':
-                    arr[idx] = (int) Math.pow(Integer.parseInt(temp), 1);
-                    idx++;
-                    temp = "";
-                    break;
-                case 'D':
-                    arr[idx] = (int) Math.pow(Integer.parseInt(temp), 2);
-                    idx++;
-                    temp = "";
-                    break;
-                case 'T':
-                    arr[idx] = (int) Math.pow(Integer.parseInt(temp), 3);
-                    idx++;
-                    temp = "";
-                    break;
-                default:
-                    temp += String.valueOf(dartResult.charAt(i));
-                    break;
+            char c = dartResult.charAt(i);
+
+            if (c == 'S') {
+                score[index] = (int) Math.pow(Double.parseDouble(temp), 1);
+                temp = "";
+                index++;
+            } else if (c == 'D') {
+                score[index] = (int) Math.pow(Double.parseDouble(temp), 2);
+                temp = "";
+                index++;
+            } else if (c == 'T') {
+                score[index] = (int) Math.pow(Double.parseDouble(temp), 3);
+                temp = "";
+                index++;
+            } else if (c == '*') {
+                score[index - 1] *= 2;
+                if (index > 1) score[index - 2] *= 2;
+            } else if (c == '#') {
+                score[index - 1] *= -1;
+            } else {
+                temp += String.valueOf(c);
             }
         }
 
         int answer = 0;
-        for (int i : arr) {
+
+        for (int i : score) {
             answer += i;
         }
 
