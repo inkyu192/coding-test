2ac16e3f0d3be716d1b7f090666c63d635bed170
inkyu192 <inkyu192@gmail.com>
2022-11-01 22:38:15 +0900

    최대공약수와 최소공배수

--- src/main/java/programmers/practice/level1/GCDAndLCM.java
+++ src/main/java/programmers/practice/level1/GCDAndLCM.java
@@ -4,11 +4,12 @@ public class GCDAndLCM {
 
     public int[] solution(int n, int m) {
         int gcd = gcd(n, m);
-        int lcm = n * m / gcd;
+        int lcm = lcm(n, m);
 
         return new int[]{gcd, lcm};
     }
 
+    // 유클리드 호제법
     private int gcd(int a, int b) {
         int max = Math.max(a, b);
         int min = Math.min(a, b);
@@ -20,5 +21,9 @@ public class GCDAndLCM {
         } else {
             return gcd(min, remainder);
         }
+    }
+
+    private int lcm(int a, int b) {
+        return a * b / gcd(a, b);
     }
 }
