c00542840601b1aed50e7c9e2971ef6e25eb3fc2
inkyu192 <inkyu192@gmail.com>
2022-11-16 23:46:16 +0900

    level0

--- src/main/java/programmers/kakao/level1/GetReportResults.java
+++ src/main/java/programmers/kakao/level1/GetReportResults.java
@@ -1,13 +1,32 @@
 package programmers.kakao.level1;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class GetReportResults {
 
     public int[] solution(String[] id_list, String[] report, int k) {
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+
+        for (String str : id_list) {
+            map.put(str, 0);
+        }
+
+        Collections.addAll(set, report);
+
+        for (String str : set) {
+            map.put(str.split(" ")[1], map.get(str.split(" ")[1]) + 1);
+        }
+
+        int[] answer = new int[id_list.length];
+
+        for (String str : set) {
+            if (map.get(str.split(" ")[1]) >= k) {
+                answer[Arrays.asList(id_list).indexOf(str.split(" ")[0])]++;
+            }
+        }
+
+        return answer;
     }
 }
