dca30b819077cec1c528d95ecf47967179634051
inkyu192 <inkyu192@gmail.com>
2023-03-13 23:42:10 +0900

    dynamic programming(동적계획법)

--- src/main/java/inflearn/introduction/section_10/problem_3/Main.java
+++ src/main/java/inflearn/introduction/section_10/problem_3/Main.java
@@ -11,6 +11,20 @@ public class Main {
 
     public int solution(int[] arr) {
         int answer = 0;
+        dy = new int[arr.length];
+        dy[0] = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+            int max = 0;
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] > arr[j]) max = Math.max(max, dy[j]);
+            }
+
+            dy[i] = max + 1;
+            answer = Math.max(answer, dy[i]);
+        }
+
         return answer;
     }
 
