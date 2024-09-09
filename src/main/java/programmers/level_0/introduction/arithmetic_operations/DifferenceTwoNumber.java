c0d4c14f964aed75105c03dafb9eb19ba39816f0
inkyu192 <inkyu192@gmail.com>
2024-09-10 00:01:08 +0900

    Revert "Day 1 사칙연산"
    This reverts commit 28954091d1f781ba174b26a93bb033c88027f86d.

--- src/main/java/programmers/level_0/introduction/arithmetic_operations/DifferenceTwoNumber.java
+++
@@ -1,8 +0,0 @@
-package programmers.level_0.introduction.arithmetic_operations;
-
-public class DifferenceTwoNumber {
-
-    public int solution(int num1, int num2) {
-        return num1 - num2;
-    }
-}
