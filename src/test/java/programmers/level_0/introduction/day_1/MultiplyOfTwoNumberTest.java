89569d97ddfaf52a3daffe650492bc032683dc18
inkyu192 <inkyu192@gmail.com>
2024-09-09 23:52:55 +0900

    Day 1 사칙연산 두 수의 합 두 수의 차 두 수의 곱 몫 구하기

--- src/test/java/programmers/level_0/introduction/day_1/MultiplyOfTwoNumberTest.java
+++ src/test/java/programmers/level_0/introduction/day_1/MultiplyOfTwoNumberTest.java
@@ -1,31 +1,31 @@
-package programmers.level_0.introduction;
+package programmers.level_0.introduction.day_1;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-@DisplayName("두 수의 차")
-class DiffOfTwoNumberTest {
+@DisplayName("두 수의 곱")
+class MultiplyOfTwoNumberTest {
 
-    DiffOfTwoNumber test = new DiffOfTwoNumber();
+    MultiplyOfTwoNumber test = new MultiplyOfTwoNumber();
 
     @Test
     void case1() {
-        int num1 = 2;
-        int num2 = 3;
+        int num1 = 3;
+        int num2 = 4;
 
-        int result = -1;
+        int result = 12;
 
         assertThat(test.solution(num1, num2)).isEqualTo(result);
     }
 
     @Test
     void case2() {
-        int num1 = 100;
-        int num2 = 2;
+        int num1 = 27;
+        int num2 = 19;
 
-        int result = 98;
+        int result = 513;
 
         assertThat(test.solution(num1, num2)).isEqualTo(result);
     }
