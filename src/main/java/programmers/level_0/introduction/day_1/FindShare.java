7c3fcf30bd79673ac254a7c839ee25cff20340d8
inkyu192 <inkyu192@gmail.com>
2024-09-10 00:01:22 +0900

    Revert "Day 1 사칙연산"
    This reverts commit 5c5ef8120b28d650bea23b7fb9e7d45a45e2eefa.

---
+++ src/main/java/programmers/level_0/introduction/day_1/FindShare.java
@@ -0,0 +1,8 @@
+package programmers.level_0.introduction.day_1;
+
+public class FindShare {
+
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
---
+++ src/test/java/programmers/level_0/introduction/day_1/MultiplyTwoNumberTest.java
@@ -0,0 +1,32 @@
+package programmers.level_0.introduction.day_1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("두 수의 곱")
+class MultiplyTwoNumberTest {
+
+    MultiplyTwoNumber test = new MultiplyTwoNumber();
+
+    @Test
+    void case1() {
+        int num1 = 3;
+        int num2 = 4;
+
+        int result = 12;
+
+        assertThat(test.solution(num1, num2)).isEqualTo(result);
+    }
+
+    @Test
+    void case2() {
+        int num1 = 27;
+        int num2 = 19;
+
+        int result = 513;
+
+        assertThat(test.solution(num1, num2)).isEqualTo(result);
+    }
+}
