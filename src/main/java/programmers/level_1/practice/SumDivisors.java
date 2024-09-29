8d30b23ae865f2bcc829ff3006c20760089ecb6b
inkyu192 <inkyu192@gmail.com>
2024-09-29 13:38:07 +0900

    약수의 합

---
+++ src/main/java/programmers/level_1/practice/SumDivisors.java
@@ -0,0 +1,16 @@
+package programmers.level_1.practice;
+
+public class SumDivisors {
+
+    public int solution(int n) {
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                result += i;
+            }
+        }
+
+        return result;
+    }
+}
