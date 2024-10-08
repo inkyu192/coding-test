4f7bbbbe0974a40bb65dc9bc683ff2ec0417fa94
inkyu192 <inkyu192@gmail.com>
2024-10-08 23:28:46 +0900

    문자 찾기

--- src/main/java/inflearn/introduction/section_1/FindString.java
+++ src/main/java/inflearn/introduction/section_1/FindString.java
@@ -1,11 +1,11 @@
-package inflearn.introduction.section_1.problem_1;
+package inflearn.introduction.section_1;
 
 import java.util.Scanner;
 
 /**
  * 1. 문자 찾기
  */
-public class Main {
+public class FindString {
 
     public int solution(String str, char t) {
         int answer = 0;
@@ -19,7 +19,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
+        FindString T = new FindString();
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
         char c = kb.next().charAt(0);
