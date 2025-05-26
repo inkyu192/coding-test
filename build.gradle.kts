c550ac7b22814c838eff54870aaf62735b18f3ca
inkyu192 <inkyu192@gmail.com>
2025-05-26 19:07:08 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.27.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
--- src/main/java/programmers/level_1/practice/ConvertStringToInteger.java
+++
@@ -1,8 +0,0 @@
-package programmers.level_1.practice;
-
-public class ConvertStringToInteger {
-
-    public int solution(String s) {
-        return Integer.parseInt(s);
-    }
-}
