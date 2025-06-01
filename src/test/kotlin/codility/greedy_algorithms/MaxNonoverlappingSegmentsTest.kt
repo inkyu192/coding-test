c386f7dc5e03e9659adadaf585c9c3a713bdf109
inkyu192 <inkyu192@gmail.com>
2025-06-01 14:11:16 +0900

    algorithms

--- src/test/kotlin/codility/greedy_algorithms/MaxNonoverlappingSegmentsTest.kt
+++ src/test/kotlin/codility/greedy_algorithms/MaxNonoverlappingSegmentsTest.kt
@@ -1,5 +1,14 @@
 package codility.greedy_algorithms
 
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
 class MaxNonoverlappingSegmentsTest {
     private val maxNonoverlappingSegments = MaxNonoverlappingSegments()
+
+    @Test
+    fun case1() {
+        val result = maxNonoverlappingSegments.solution(intArrayOf(1, 3, 7, 9, 9), intArrayOf(5, 6, 8, 9, 10))
+        Assertions.assertThat(result).isEqualTo(3)
+    }
 }
